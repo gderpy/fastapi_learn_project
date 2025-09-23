5b1bc67c6fd9c8da115d0c40cf46b6cfb9bdc797
Yury Petrov <gderpov@yandex.ru>
2025-09-23 21:45:50 +0500

    feature(db): Инициализация БД через lifespan, экспорт db_helper - Подключение db_helper и DataBaseHelper в core/models/__init__.py - Добавление lifespan в main.py, чтобы при старте приложения автоматически создавать таблицы через Base.metadata.create_all

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,5 @@
 from .base import Base 
 from .product import Product
+from .db_helper import db_helper, DataBaseHelper
 
-__all__ = ["Base", "Product"]
+__all__ = ["Base", "Product", "db_helper", "DataBaseHelper"]
