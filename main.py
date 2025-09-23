98546da023c9d0d4366eb63125250bdec97724c3
Yury Petrov <gderpov@yandex.ru>
2025-09-23 20:22:41 +0500

    FastAPI: Часть 2 - Pydantic - APIRouter - CRUD

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import uvicorn
-from fastapi import FastAPI
+
+from typing import Annotated
+
+from fastapi import FastAPI, Path
 from pydantic import EmailStr, BaseModel
 
-app = FastAPI()
+from items_views import router as items_router
+from users.views import router as users_router
 
-class CreateUser(BaseModel):
-    email: EmailStr
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -19,39 +24,12 @@ def hello(name: str = "World"):
     return {"message": f"Hello, {name}!"}
 
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "message": "success",
-        "email": user.email,
-    }
-
-
 @app.post("/calc/add/")
 def add(a: int, b: int):
     return {
         "a": a,
         "b": b,
         "result": a + b,
-    }
-
-
-@app.get("/items/")
-def list_items():
-    return [
-        "Item1", "Item2", "Item3"
-    ]
-
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"item": {"id": "0", "name": "latest"}}
-
-
-@app.get("/items/{item_id}")
-def get_item_by_id(item_id: int):
-    return {
-        "item": {"id": item_id}
     }
 
 
