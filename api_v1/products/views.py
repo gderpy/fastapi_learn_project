8a5d6d8c04c2a4d7d322696b86d6cba7e6732136
Yury Petrov <gderpov@yandex.ru>
2025-09-24 15:57:06 +0500

    feature(api-views): Валидированы ответы и возврат 404 при отсутствии продукта

--- api_v1/products/views.py
+++ api_v1/products/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from . import crud
 from .schemas import Product, ProductCreate
@@ -6,19 +6,28 @@ from .schemas import Product, ProductCreate
 
 router = APIRouter(tags=["Products"])
 
+# Для эндпоинтов добавлен response_model: list[Product], Product
+# Теперь FastAPI будет автоматически валидировать и возвращать объекты в нужном формате
 
-@router.get("/")
+@router.get("/", response_model=list[Product])
 async def get_products(session):
     return await crud.get_products(session)
 
 
-@router.post("/")
+@router.post("/", response_model=Product)
 async def create_product(session, product_in: ProductCreate):
     return await crud.create_product(session=session, product_in=product_in)
 
 
-@router.get("/{product_id}/")
+@router.get("/{product_id}/", response_model=Product)
 async def get_product(session, product_id: int):
-    return await crud.get_product(session=session, product_id=product_id)
+    product = await crud.get_product(session=session, product_id=product_id)
 
+    if product is not None:
+        return product
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Product {product_id} not found!"
+    )
 
