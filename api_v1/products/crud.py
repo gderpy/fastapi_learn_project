6c83495f8e9dffdf0f0161e9387fc9bf0ef59dcd
Yury Petrov <gderpov@yandex.ru>
2025-09-24 15:36:23 +0500

    feature(products): Реализован асинхронный метод - CRUD для создания и получения продукта - create_product

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -3,6 +3,7 @@ from sqlalchemy.engine import Result
 from sqlalchemy import select
 
 from core.models.product import Product
+from .schemas import ProductCreate
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
@@ -14,3 +15,11 @@ async def get_products(session: AsyncSession) -> list[Product]:
 
 async def get_product(session: AsyncSession, product_id: int) -> Product | None:
     return await session.get(Product, product_id)
+
+
+async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
+    product = Product(**product_in.model_dump())
+    session.add(product)
+    await session.commit()
+    # await session.refresh(product)
+    return product
