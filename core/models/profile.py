49e5bbab825d6e71e113b600773179918cbc062e
Yury Petrov <gderpov@yandex.ru>
2025-09-25 18:48:58 +0500

    refactor(models/profile, user): Подключен общий миксин и связка User с Profile - Profile наследует UserRelationMixin, задаёт _user_id_unique = True и _user_back_populates = "profile", чтобы автоматом получить уникальный user_id и relationship. - В User добавлено поле profile с двусторонней связью relationship(back_populates="user")

--- core/models/profile.py
+++ core/models/profile.py
@@ -4,14 +4,18 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Profile(Base):
+class Profile(Base, UserRelationMixin):
+    _user_id_unique = True 
+    _user_back_populates = "profile"
+
     first_name: Mapped[str | None] = mapped_column(String(64))
     last_name: Mapped[str | None] = mapped_column(String(64))
     bio: Mapped[str | None]
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    
