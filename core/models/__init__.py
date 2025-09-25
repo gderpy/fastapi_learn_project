c22f3d9fa78a882722e94050bcbf8202af76a837
Yury Petrov <gderpov@yandex.ru>
2025-09-25 18:09:27 +0500

    feature(alembic, models): Создана модель Post, создана и проведена миграция - модель имеет внешний ключ на users.id

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,10 @@
 from .base import Base 
 from .product import Product
 from .user import User
+from .post import Post
 from .db_helper import db_helper, DataBaseHelper
 
-__all__ = ["Base", "User", "Product", "db_helper", "DataBaseHelper"]
+__all__ = [
+    "Base", 
+    "User", "Post", "Product", 
+    "db_helper", "DataBaseHelper"]
