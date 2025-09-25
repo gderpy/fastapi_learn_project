647e057423c5b92da7a28eebf85bac76b5d05db0
Yury Petrov <gderpov@yandex.ru>
2025-09-25 18:17:43 +0500

    feature(models): Связка моделей User и Post через relationship - в Post поле user -> posts - в User поле posts (список post): post -> user

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -9,3 +14,4 @@ class Post(Base):
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
