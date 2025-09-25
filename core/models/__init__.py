ebf8c188c097f77216d04be6e3a6f7872431c9db
Yury Petrov <gderpov@yandex.ru>
2025-09-25 18:54:07 +0500

    refactor(models, post): Добавлен Profile в общий импорт и убрано лишний в post

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,9 +2,10 @@ from .base import Base
 from .product import Product
 from .user import User
 from .post import Post
+from .profile import Profile
 from .db_helper import db_helper, DataBaseHelper
 
 __all__ = [
     "Base", 
-    "User", "Post", "Product", 
+    "User", "Post", "Product", "Profile", 
     "db_helper", "DataBaseHelper"]
--- core/models/post.py
+++ core/models/post.py
@@ -1,17 +1,12 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
 
 from .base import Base
 from .mixins import UserRelationMixin
 
-if TYPE_CHECKING:
-    from .user import User
-
 
 class Post(Base, UserRelationMixin):
     _user_back_populates = "posts"
-    
+
     title: Mapped[str] = mapped_column(String(128))
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
