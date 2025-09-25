38682a7497d60b640e356b2921e51993dada12db
Yury Petrov <gderpov@yandex.ru>
2025-09-25 18:44:11 +0500

    refactor(models/post): Использован общий миксин свящи с User в Post - класс Post наследует UserRelationMixin, непосредственно объявленные поля user_id и user удалены - прописывается _user_back_populates = "posts", чтобы миксин настроил relationship на сторону User

--- core/models/post.py
+++ core/models/post.py
@@ -4,14 +4,14 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Post(Base):
+class Post(Base, UserRelationMixin):
+    _user_back_populates = "posts"
+    
     title: Mapped[str] = mapped_column(String(128))
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user: Mapped["User"] = relationship(back_populates="posts")
