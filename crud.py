3d8c775f30bef2d768d2207de0ccc02c6519b253
Yury Petrov <gderpov@yandex.ru>
2025-09-26 14:40:04 +0500

    feature(crud): Добавлен пример асинхронного создания пользователей

---
+++ crud.py
@@ -0,0 +1,22 @@
+import asyncio
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, User, Profile, Post
+
+
+async def create_user(session: AsyncSession, username: str) -> User:
+    user = User(username=username)
+    session.add(user)
+    await session.commit()
+    print("user", user)
+    return user
+
+
+async def main():
+    async with db_helper.session_factory() as session:
+        await create_user(session=session, username="John")
+        await create_user(session=session, username="Sam")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
