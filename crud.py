cbbdef9b8e5a5382210c6779633816e5c65326f6
Yury Petrov <gderpov@yandex.ru>
2025-09-26 15:18:50 +0500

    feature(crud): Добавлен поиск пользователя и отключен SQL-лог - (crud.py): подключены select и Result, реализована функция get_user_by_username, которая вытягивает пользователя по имени и печатает результат. В main() показано, как можно вызвать поиск на примере Sam и Bob, а создание новых пользователей оставлено закомментированным.

--- crud.py
+++ crud.py
@@ -1,5 +1,7 @@
 import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 from core.models import db_helper, User, Profile, Post
 
@@ -12,10 +14,20 @@ async def create_user(session: AsyncSession, username: str) -> User:
     return user
 
 
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    result: Result = await session.execute(stmt)
+    user: User | None = result.scalar_one_or_none()
+    print(f"\nfound user {username}, {user}\n")
+    return user
+
+
 async def main():
     async with db_helper.session_factory() as session:
-        await create_user(session=session, username="John")
-        await create_user(session=session, username="Sam")
+        # await create_user(session=session, username="John")
+        # await create_user(session=session, username="Sam")
+        await get_user_by_username(session=session, username="Sam")
+        await get_user_by_username(session=session, username="Bob")
 
 
 if __name__ == "__main__":
