730e8595b08227c79406eeab408cf071870ad3bf
Yury Petrov <gderpov@yandex.ru>
2025-09-29 17:42:59 +0500

    feature(auth): Расширены эндпоинты для Basic Auth - подключили secrets, HTTPException, status для безопасной проверки. - добавили словарь тестовых пользователей и функцию get_auth_user_username, бросающую 401 при неверных данных и возвращающую имя через безопасное сравнение паролей. - создали новый GET /api/v1/demo-auth/basic-auth-username/, который отвечает приветствием и именем авторизованного пользователя.

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,6 +1,8 @@
+import secrets
+
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 
@@ -17,4 +19,43 @@ def demo_basic_auth_credentials(
         "message": "Hi!",
         "username": credentials.username,
         "password": credentials.password, 
+    }
+
+
+usernames_to_password = {
+    "admin": "admin",
+    "john": "password",
+}
+
+
+def get_auth_user_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+
+    correct_password = usernames_to_password.get(credentials.username)
+
+    if correct_password is None:
+        raise unauthed_exc
+    
+    if not secrets.compare_digest(
+        credentials.password.encode("utf-8"),
+        correct_password.encode("utf-8"),
+    ):
+        raise unauthed_exc
+    
+    return credentials.username
+
+
+@router.get("/basic-auth-username/")
+def demo_basic_auth_username(
+    auth_username: str = Depends(get_auth_user_username),
+    ):
+    return {
+        "message": f"Hi, {auth_username}!",
+        "username": auth_username,
     }
